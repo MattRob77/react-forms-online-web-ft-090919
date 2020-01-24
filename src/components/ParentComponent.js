d0d082b5ce28daf65d412fd7b7b0b4c3607a1be0
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-24 19:36:21 +0000

    Automatically backed up by Learn

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from './Form'
+
+class ParentComponent extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
+  
+  handleFirstnameChange 
+}
