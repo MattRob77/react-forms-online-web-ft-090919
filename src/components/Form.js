65d4dfd6e6025e25d578f06d2254e3c624e26c46
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-24 19:22:39 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -18,14 +18,14 @@ class Form extends React.Component {
     })
   }
 
-  render() {
+   render() {
     return (
       <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+        <input type="text" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+        <input type="text" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
     )
   }
 }
-
+ 
 export default Form;
